77919918a0e3493c76fa3970cce7bff47711e0e7
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-05-20 15:15:01 -0700

    Code for finding nemo 2

---
+++ 00_bigO/findNemo2.js
@@ -0,0 +1,29 @@
+const nemo = ['nemo']
+const everyone = [
+  'dory',
+  'bruce',
+  'marlin',
+  'nemo',
+  'gill',
+  'bloat',
+  'nigel',
+  'squirt',
+  'darla',
+  'hank',
+]
+
+const large = new Array(10000).fill('nemo')
+
+function findNemo(array) {
+  let t0 = performance.now()
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('Found Nemo!')
+    }
+  }
+  let t1 = performance.now()
+  // console.log('Call to find  took ' + (t1 - t0) + 'milliseconds')
+  console.log(`Call to find the array took ${t1 - t0} miliseconds`)
+}
+
+findNemo(large)
