ce6ef3f3bcee4797db586b7ab2409a9ab2c484a6
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-05-26 17:42:26 +0900

    Slight adjustments to the find nemo exercise

--- 00_bigO/findNemo2.js
+++ 00_bigO/findNemo2.js
@@ -3,23 +3,25 @@ const everyone = [
   'dory',
   'bruce',
   'marlin',
-  'nemo',
   'gill',
   'bloat',
   'nigel',
   'squirt',
   'darla',
   'hank',
+  'nemo',
 ]
 
 const large = new Array(10000).fill('nemo')
 
 function findNemo(array) {
   for (let i = 0; i < array.length; i++) {
+    console.log('running')
     if (array[i] === 'nemo') {
       console.log('Found Nemo!')
+      break
     }
   }
 }
 
-findNemo(large) // 0(n) --> Linear Time, it takes linear time to find nemo
+findNemo(everyone) // 0(n) --> Linear Time, it takes linear time to find nemo
