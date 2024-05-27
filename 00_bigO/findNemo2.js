68403525e88664c35e7bc08023ca2e088d2da2c4
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-05-27 14:33:30 +0800

    Addtional ways of writing loops

--- 00_bigO/findNemo2.js
+++ 00_bigO/findNemo2.js
@@ -25,3 +25,23 @@ function findNemo(array) {
 }
 
 findNemo(everyone) // 0(n) --> Linear Time, it takes linear time to find nemo
+
+const findNemo2 = array => {
+  array.forEach(fish => {
+    if (fish === 'nemo') {
+      console.log('Found Nemo!')
+    }
+  })
+}
+
+// Another way of writing findNemo2 function
+const findNemo3 = array => {
+  for (let fish of array) {
+    if (fish === 'nemo') {
+      console.log('Found nemo')
+    }
+  }
+}
+
+findNemo2(everyone) // O(n)
+findNemo3(everyone) // O(n)
