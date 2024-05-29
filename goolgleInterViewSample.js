1c7d9250fa84757a6921ae3379937a60f50eb3f0
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-05-29 16:40:58 +0800

    Simplify code further

--- goolgleInterViewSample.js
+++ goolgleInterViewSample.js
@@ -12,6 +12,7 @@ const array2 = ['z', 'y', 'a']
 
 function containsCommonItem2(arr1, arr2) {
   // loop through first array and create object where properties === items in the array
+  // can we always assume 2 params being passed to the function?
   let map = {}
   for (let i = 0; i < arr1.length; i++) {
     if (!map[arr1[i]]) {
@@ -19,6 +20,7 @@ function containsCommonItem2(arr1, arr2) {
       map[item] = true
     }
   }
+
   // loop through the second array and check if item in second array exists on created object
   for (let j = 0; j < arr2.length; j++) {
     if (map[arr2[j]]) {
@@ -30,5 +32,14 @@ function containsCommonItem2(arr1, arr2) {
   return false
 }
 
-// O(a + b) Time complexity
-containsCommonItem2(array1, array2)
+// O(a + b)
+const hello = containsCommonItem2(array1, array2)
+
+function containsCommonItems3(arr1, arr2) {
+  return arr1.some(item => arr2.includes(item))
+}
+
+const hello2 = containsCommonItems3(array1, array2)
+
+console.log(hello, 'hello')
+console.log(hello2, 'hello2')
