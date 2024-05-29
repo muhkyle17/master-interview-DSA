59e0f00b5bd91753727b0dc85859a05aaec1fbdc
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-05-29 17:00:27 +0800

    Final code for the google interview example

--- goolgleInterViewSample.js
+++ goolgleInterViewSample.js
@@ -1,18 +1,34 @@
+// Given 2 arrays, create a function that let's a user know (true/false) whether these two arrays contain any common items
+//For Example:
+//const array1 = ['a', 'b', 'c', 'x'];//const array2 = ['z', 'y', 'i'];
+//should return false.
+//-----------
+//const array1 = ['a', 'b', 'c', 'x'];//const array2 = ['z', 'y', 'x'];
+//should return true.
+
+// 2 parameters - arrays - no size limit
+// return true or false
+
+function containsCommonItem(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        return true
+      }
+    }
+  }
+  return false
+}
+
+//O(a*b)
+//O(1) - Space Complexity
+
 const array1 = ['a', 'b', 'c', 'x']
 const array2 = ['z', 'y', 'a']
 
-// what we want to do
-// array1 ==> obj {
-// a: true,
-// b: true,
-// c: true,
-// x: true,
-// }
-// array2[index] === obj.properties
-
 function containsCommonItem2(arr1, arr2) {
   // loop through first array and create object where properties === items in the array
-  // can we always assume 2 params being passed to the function?
+  // can we assume always 2 params?
   let map = {}
   for (let i = 0; i < arr1.length; i++) {
     if (!map[arr1[i]]) {
@@ -21,25 +37,24 @@ function containsCommonItem2(arr1, arr2) {
     }
   }
 
-  // loop through the second array and check if item in second array exists on created object
+  // loop through second array and check if item in second array exists on created object.
   for (let j = 0; j < arr2.length; j++) {
     if (map[arr2[j]]) {
-      console.log(true, 'true')
       return true
     }
   }
-  console.log(false, 'false')
   return false
 }
 
-// O(a + b)
-const hello = containsCommonItem2(array1, array2)
+//O(a + b) Time Complexity
+//O(a) Space Complexity
 
-function containsCommonItems3(arr1, arr2) {
+// containsCommonItem2(array1, array2)
+
+function containsCommonItem3(arr1, arr2) {
   return arr1.some(item => arr2.includes(item))
 }
 
-const hello2 = containsCommonItems3(array1, array2)
-
-console.log(hello, 'hello')
-console.log(hello2, 'hello2')
+containsCommonItem(array1, array2)
+containsCommonItem2(array1, array2)
+containsCommonItem3(array1, array2)
