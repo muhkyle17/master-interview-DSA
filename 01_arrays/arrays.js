a550fc2c72b97624093582dd89e530d9228ffff3
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-06-08 01:42:15 -0700

    Add unshift and splice

--- 01_arrays/arrays.js
+++ 01_arrays/arrays.js
@@ -10,4 +10,10 @@ strings.push('e') // O(1)
 strings.pop('') // O(1)
 strings.pop('') // O(1)
 
+// unshift -- Add an item at the beginning of the array
+strings.unshift('x') // O(n) -- this is because we have to shift all the items to the right so it's looping
+
+// splice -- Add an item at the middle of the array
+strings.splice(2, 0, 'alien') // O(n/2) -- O(n)
+
 console.log(strings, 'strings')
