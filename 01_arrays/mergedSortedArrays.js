c18ca8c1d3ee634cf2d999454260e26a90ed4fa5
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-06-10 16:59:45 -0700

    Remove checks since it's not needed

--- 01_arrays/mergedSortedArrays.js
+++ 01_arrays/mergedSortedArrays.js
@@ -23,15 +23,7 @@ function mergeSortedArrays(array1, array2) {
 const sortedArray = mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]) // [0, 3, 4, 4, 6, 30, 31];
 console.log(sortedArray, 'sortedArray')
 
-function mergedSortedArrays2(array1, array2) {
-  // Check input
-  if (array1.length === 0) {
-    return array2
-  }
-  if (array2.length === 0) {
-    return array1
-  }
-
+function mergeSortedArrays2(array1, array2) {
   let mergedArray = [...array1, ...array2]
   return mergedArray.sort((a, b) => a - b)
 }
