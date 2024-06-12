30c4afd0eaae7812cd272f835482a125495818f1
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-06-12 12:32:41 -0700

    Add the get function

--- 02_hashTables/hashTableExercise.js
+++ 02_hashTables/hashTableExercise.js
@@ -7,28 +7,44 @@ class HashTable {
     let hash = 0
     for (let i = 0; i < key.length; i++) {
       hash = (hash + key.charCodeAt(i) * i) % this.data.length
-      console.log(hash, 'hash')
     }
     return hash
-  }
+  } // O(1)
 
   set(key, value) {
     let address = this._hash(key)
     if (!this.data[address]) {
       this.data[address] = []
       this.data[address].push([key, value])
-      console.log(this.data, 'this.data')
+      // console.log(this.data, "this.data");
     }
     this.data[address].push([key, value])
     return this.data
-  }
+  } // O(1)
+
+  get(key) {
+    let address = this._hash(key)
+    const currentBucket = this.data[address]
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1]
+        }
+      }
+    }
+    return undefined
+  } // if no collisions, O(1) but if there are collisions, O(n)
 }
 
-const myHashTable = new HashTable(2)
+const myHashTable = new HashTable(50)
 const exampleHash = myHashTable._hash('grapes')
-console.log(exampleHash, 'exampleHash')
+// console.log(exampleHash, "exampleHash");
 
 const setFunction = myHashTable.set('grapes', 10000)
 const setFunctionApples = myHashTable.set('apples', 54)
-console.log(setFunctionApples, 'setFunction')
-// myHashTable.get("grapes");
+// console.log(setFunctionApples, "setFunction");
+
+const getFunctionGrapes = myHashTable.get('grapes')
+console.log(getFunctionGrapes, 'getFunctionGrapes')
+const getFunctionApples = myHashTable.get('apples')
+console.log(getFunctionApples, 'getFunctionApples')
