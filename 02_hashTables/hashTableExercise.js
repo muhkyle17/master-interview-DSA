3b01faaceb8bd46bf66eb0851e00162005cf0e19
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-06-12 00:09:06 -0700

    Add set function

--- 02_hashTables/hashTableExercise.js
+++ 02_hashTables/hashTableExercise.js
@@ -7,13 +7,28 @@ class HashTable {
     let hash = 0
     for (let i = 0; i < key.length; i++) {
       hash = (hash + key.charCodeAt(i) * i) % this.data.length
+      console.log(hash, 'hash')
     }
     return hash
   }
+
+  set(key, value) {
+    let address = this._hash(key)
+    if (!this.data[address]) {
+      this.data[address] = []
+      this.data[address].push([key, value])
+      console.log(this.data, 'this.data')
+    }
+    this.data[address].push([key, value])
+    return this.data
+  }
 }
 
-const myHashTable = new HashTable(50)
-// myHashTable.set("grapes", 10000);
+const myHashTable = new HashTable(2)
+const exampleHash = myHashTable._hash('grapes')
+console.log(exampleHash, 'exampleHash')
+
+const setFunction = myHashTable.set('grapes', 10000)
+const setFunctionApples = myHashTable.set('apples', 54)
+console.log(setFunctionApples, 'setFunction')
 // myHashTable.get("grapes");
-const hello = myHashTable._hash('grapesasdfasdfsf')
-console.log(hello, 'hello')
