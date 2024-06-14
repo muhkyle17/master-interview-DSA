277875d6926ee9d0c9837d6d8e0c01fdbb64bb91
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-06-13 23:02:07 -0700

    Linked lists intro pseudo code

---
+++ 03_linkedLists/introCode.js
@@ -0,0 +1,8 @@
+const basket = ['apples', 'grapes', 'pears']
+
+// linked list: apples linksto--> grapes linksto--> pears
+
+// apples
+// 8947 --> grapes
+//           8742 --> pears
+//                     372 --> null
