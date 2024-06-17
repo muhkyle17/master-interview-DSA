a38a3b79d8b5bf5f7a138a8e8e7b8e8c0f418fd8
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-06-16 17:49:14 -0700

    Add the append method

--- 03_linkedLists/linkedListImplementation.js
+++ 03_linkedLists/linkedListImplementation.js
@@ -22,7 +22,22 @@ class LinkedList {
     this.tail = this.head
     this.length = 1
   }
+
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    }
+    this.tail.next = newNode
+    this.tail = newNode
+    this.length++
+    console.log(this, 'this')
+    return this
+  }
 }
 
 const myLinkedList = new LinkedList(10)
+myLinkedList.append(5)
+myLinkedList.append(16)
 console.log(myLinkedList, 'myLInkedList}')
+console.log(myLinkedList.head.next.next, 'head')
