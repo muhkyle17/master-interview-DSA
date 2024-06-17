396503eacb4a0b5c95f8c6e114d2539077a9f9f1
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-06-16 17:17:14 -0700

    Some notes on linked lists

--- 03_linkedLists/introCode.js
+++ 03_linkedLists/introCode.js
@@ -6,3 +6,10 @@ const basket = ['apples', 'grapes', 'pears']
 // 8947 --> grapes
 //           8742 --> pears
 //                     372 --> null
+
+let obj1 = { a: true }
+let obj2 = obj1
+obj1.a = 'booya'
+delete obj1
+console.log(obj1, 'obj1')
+console.log(obj2, 'obj2')
