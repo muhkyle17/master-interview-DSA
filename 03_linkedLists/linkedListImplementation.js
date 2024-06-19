221af78bbf6d015bb3dcec7b1d211f0a837f984a
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-06-18 20:43:57 -0700

    Add a print list function, insert functin and tranvertoindex function

--- 03_linkedLists/linkedListImplementation.js
+++ 03_linkedLists/linkedListImplementation.js
@@ -49,11 +49,59 @@ class LinkedList {
     this.length++
     return this
   }
+
+  printList() {
+    const array = []
+    let currentNode = this.head
+    while (currentNode !== null) {
+      array.push(currentNode.value)
+      currentNode = currentNode.next
+    }
+    return array
+  }
+
+  // 1-->10-->99-->5-->16
+  insert(index, value) {
+    // Check for proper parameters;
+    if (index >= this.length) {
+      return this.append(value)
+    }
+
+    // if (index === 0) {
+    //   this.prepend(value);
+    //   return this.printList();
+    // }
+
+    const newNode = {
+      value: value,
+      next: null,
+    }
+
+    const leader = this.traverseToIndex(index - 1)
+    const holdingPointer = leader.next
+    leader.next = newNode
+    newNode.next = holdingPointer
+    this.length++
+    return this.printList()
+  }
+
+  traverseToIndex(index) {
+    let counter = 0
+    let currentNode = this.head
+    while (counter !== index) {
+      currentNode = currentNode.next
+      counter++
+    }
+    return currentNode
+  }
 }
 
 const myLinkedList = new LinkedList(10)
 myLinkedList.append(5)
 myLinkedList.append(16)
 myLinkedList.prepend(1)
+myLinkedList.insert(2, 99)
+myLinkedList.insert(20, 88)
+console.log(myLinkedList.printList(), 'printlist function')
+
 console.log(myLinkedList, 'myLinkedList')
-console.log(myLinkedList.head.next, 'myLinkedList.head.next')
