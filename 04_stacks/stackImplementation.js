381827f9fdf938873a4973395c5b4c3a5fab1a20
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-06-27 11:47:57 -0700

    Add initial code for exercise

---
+++ 04_stacks/stackImplementation.js
@@ -0,0 +1,20 @@
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null
+    this.bottom = null
+    this.length = 0
+  }
+
+  peek() {}
+
+  push(value) {}
+
+  pop() {}
+}
